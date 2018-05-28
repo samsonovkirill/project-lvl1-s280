9ac6ce22ac0e68f9796df7f5dd387d2d760e5450
Kirill Samsonov <itsamsonovkirill@gmail.com>
2018-05-28 20:52:56 +0300

    checkpoint2.1

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import readlineSync from 'readline-sync';
 export default function() {
   console.log('Welcome to the Brain Games!');
   let userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
+  console.log(`Hello, ${userName}!`);
 }
