340c7666bd62eb609b04b124b00b6008aee6056b
Kirill Samsonov <itsamsonovkirill@gmail.com>
2018-05-29 21:21:10 +0300

    checkpoint4

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -2,25 +2,37 @@
 import readlineSync from 'readline-sync';
 import { welcome, sayHi, congratulate } from '..';
 
+const ROUNDS = 3;
 
-(function startGame() {
+(function Game(rounds) {
   const lineTranslation = () => {
     console.log('\n');
   };
-  const playRound = () => {
+  const playRound = (username) => {
     const testNumber = Math.floor(Math.random() * 100);
     console.log(`Question: ${testNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    const rightAnswer = (parseInt(testNumber, 10) % 2 === 0) ? 'yes' : 'no';
-    return userAnswer === rightAnswer;
+    const correctAnswer = (testNumber % 2 === 0) ? 'yes' : 'no';
+    const isCorrect = userAnswer.toLowerCase() === correctAnswer;
+    const userMessage = isCorrect ? 'Correct!' : `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n Let's try again, ${username}!`;
+    console.log(userMessage);
+    return isCorrect;
   };
   welcome();
   console.log('Answer "yes" if number even otherwise answer "no".');
   lineTranslation();
   const username = sayHi();
   lineTranslation();
-  const result = playRound();
-  if (result) {
+  let roundsCount = rounds;
+  while (roundsCount !== 0) {
+    const isCorrect = playRound(username);
+    if (isCorrect) {
+      roundsCount -= 1;
+    } else {
+      break;
+    }
+  }
+  if (roundsCount === 0) {
     congratulate(username);
   }
-}());
+}(ROUNDS));
