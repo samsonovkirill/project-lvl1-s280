23d52e23aee408038e15045eb9783a5bd664c92b
Kirill Samsonov <itsamsonovkirill@gmail.com>
2018-06-02 16:58:20 +0300

    fix prime

--- src/mini-games/prime.js
+++ src/mini-games/prime.js
@@ -7,7 +7,7 @@ const isPrime = (num) => {
   if (num === 2) {
     return true;
   }
-  if (num === 1 || num % 2 === 0) {
+  if (num <= 1 || num % 2 === 0) {
     return false;
   }
   const max = Math.round(Math.sqrt(num));
